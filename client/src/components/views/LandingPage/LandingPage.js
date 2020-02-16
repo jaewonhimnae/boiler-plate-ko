b348a3eb41da7806b9708606304ccbcfb62ccb43
John Ahn <smileajw1004@gmail.com>
2020-02-16 10:23:55 +0900

    Set up proxy

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{ useEffect} from 'react'
+import axios from 'axios';
 
 function LandingPage() {
+
+     useEffect(() => {
+        axios.get('/api/hello')
+        .then(response=> { console.log(response)})
+
+     }, [])
+
     return (
         <div>
             LandingPage  랜딩페이지
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -21,10 +20,9 @@ mongoose.connect(config.mongoURI, {
   .catch(err => console.log(err))
 
 
-
-
 app.get('/', (req, res) => res.send('Hello World!~~ '))
 
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
 
 app.post('/api/users/register', (req, res) => {
 
@@ -109,5 +107,6 @@ app.get('/api/users/logout', auth, (req, res) => {
 
 
 
+const port = 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
