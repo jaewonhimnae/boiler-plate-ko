93b47f077d5868f61a17d7faa9adc7ffb8cd5fac
John Ahn <smileajw1004@gmail.com>
2020-03-14 22:56:56 +0900

    Log Out Added

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,24 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
             .then(response => { console.log(response) })
     }, [])
+
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push("/login")
+                } else {
+                    alert('로그아웃 하는데 실패 했습니다.')
+                }
+            })
+    }
 
     return (
         <div style={{
@@ -14,6 +26,11 @@ function LandingPage() {
             , width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
+
         </div>
     )
 }
