e8b6a4d251c320444b2c8e78c2dd9cc89db52e15
John Ahn <smileajw1004@gmail.com>
2020-03-22 10:46:08 +0900

    Fixed the proxy issue

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,9 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function(app) {
   app.use(
     '/api',
-    proxy({
+    createProxyMiddleware({
       target: 'http://localhost:5000',
       changeOrigin: true,
     })
